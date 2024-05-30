0e2c4c8cda340c07b0c8257739e0cb1c17700391
BugGreen <nirojasbe@unal.edu.co>
2024-05-30 18:28:55 +0200

    FEAT: create static method `get_state` to binary encode the state of the game.
    get_sate encodes information about the game environment. It has eleven possibilities encoded in boolean values [danger_straight, danger_rigth, danger_left, direction_left, direction_rigth, direction_down, reward_left, reward_right, reward_up, reward_down]

--- agent.py
+++ agent.py
@@ -2,6 +2,8 @@ import torch
 import random
 import numpy as np
 from collections import deque
+
+import snake_game
 from snake_game import SnakeGameAI, Direction, Point
 
 MAX_MEMORY = 100_000
@@ -18,8 +20,99 @@ class Agent:
         self.memory = deque(maxlen=MAX_MEMORY)  # If we exceed the memory, it will remove elements from the left
         # TODO: model & training
 
-    def get_state(self, game):
-        pass
+    @staticmethod
+    def get_state(game):
+        """
+        There are 11 possible values that encode where a collision would occur, the snake direction and the position of
+        the food with respect to the snake.
+        :param game: an instance of a SnakeGameAI
+        :return: binary encoding of the current game state
+        """
+        def danger_straight(right_direction: bool, left_direction: bool, up_direction: bool, down_direction: bool,
+                            point_r: Point, point_l: Point, point_u: bool, point_d: Point,
+                            game_object: SnakeGameAI = game) -> bool:
+            """
+            Determines whether a danger is placed on the current direction of the snake.
+
+            :param right_direction: boolean that encodes if the current snake directions is `right`
+            :param left_direction: boolean that encodes if the current snake directions is `left`
+            :param up_direction: boolean that encodes if the current snake directions is `up`
+            :param down_direction: boolean that encodes if the current snake directions is `down`
+            :param point_r: coordinates of the right boundary of the snake head
+            :param point_l: coordinates of the left boundary of the snake head
+            :param point_u: coordinates of the upper boundary of the snake head
+            :param point_d: coordinates of the lower boundary of the snake head
+            :param game_object: an instance of a SnakeGameAI
+
+            :return: boolean value encoding if a danger is placed on the current direction
+            """
+            danger_is_straight = (right_direction and game_object.is_collision(point_r)) or \
+                                 (left_direction and game_object.is_collision(point_l)) or \
+                                 (up_direction and game_object.is_collision(point_u)) or \
+                                 (down_direction and game_object.is_collision(point_d))
+
+            return danger_is_straight
+
+        def danger_right(right_direction: bool, left_direction: bool, up_direction: bool, down_direction: bool,
+                         point_r: Point, point_l: Point, point_u: bool, point_d: Point,
+                         game_object: SnakeGameAI = game) -> bool:
+            """
+            Determines whether a danger is right placed with respect to the current direction of the snake
+
+            :return: boolean value encoding if a danger is placed on the right of the current direction
+            """
+            danger_is_right = (right_direction and game_object.is_collision(point_d)) or \
+                              (left_direction and game_object.is_collision(point_u)) or \
+                              (up_direction and game_object.is_collision(point_r)) or \
+                              (down_direction and game_object.is_collision(point_l))
+
+            return danger_is_right
+
+        def danger_left(right_direction: bool, left_direction: bool, up_direction: bool, down_direction: bool,
+                         point_r: Point, point_l: Point, point_u: bool, point_d: Point,
+                         game_object: SnakeGameAI = game) -> bool:
+            """
+            Determines whether a danger is left placed with respect to the current direction of the snake
+
+            :return: boolean value encoding if a danger is placed on the left of the current direction
+            """
+            danger_is_right = (right_direction and game_object.is_collision(point_u)) or \
+                              (left_direction and game_object.is_collision(point_d)) or \
+                              (up_direction and game_object.is_collision(point_l)) or \
+                              (down_direction and game_object.is_collision(point_r))
+
+            return danger_is_right
+
+        head = game.snake[0]
+
+        # Coordinates of the head boundaries to check where is the danger with repect to it
+        point_left = Point(head.x - 20, head.y)
+        point_right = Point(head.x + 20, head.y)
+        point_up = Point(head.x, head.y - 20)
+        point_down = Point(head.x, head.y + 20)
+
+        # Encode the current snake direction
+        dir_left = game.direction == Direction.LEFT
+        dir_right = game.direction == Direction.RIGHT
+        dir_up = game.direction == Direction.UP
+        dir_down = game.direction == Direction.DOWN
+
+        state = [danger_straight(dir_right, dir_left, dir_up, dir_down, point_right, point_left, point_up, point_down),
+                 danger_right(dir_right, dir_left, dir_up, dir_down, point_right, point_left, point_up, point_down),
+                 danger_left(dir_right, dir_left, dir_up, dir_down, point_right, point_left, point_up, point_down),
+                 # Move direction encoding:
+                 dir_left,
+                 dir_right,
+                 dir_up,
+                 dir_down,
+                 # Reward (food) location:
+                 game.food.x < game.head.x,  # Reward to the left
+                 game.food.x > game.head.x,  # Reward to the right
+                 game.food.y < game.head.y,  # Reward above
+                 game.food.y > game.head.y,  # Reward below
+                 ]
+
+        return np.array(state, dtype=int)  # Convert boolean values to binary
 
     def remember(self, state, action, reward, next_state, game_over):
         pass
